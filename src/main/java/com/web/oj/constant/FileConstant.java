c919d4585ba36123b1f2f6ab3e8c072b3d59bbfb
as_before <1359521868@qq.com>
2024-09-03 17:38:51 +0800

    Init commit

---
+++ src/main/java/com/web/oj/constant/FileConstant.java
@@ -0,0 +1,16 @@
+package com.web.oj.constant;
+
+/**
+ * 文件常量
+ *
+ * @author <a href="https://github.com/liweb">程序员鱼皮</a>
+ * @from <a href="https://web.icu">编程导航知识星球</a>
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://web.icu";
+}
