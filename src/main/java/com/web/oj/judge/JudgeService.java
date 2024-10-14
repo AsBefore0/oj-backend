67bb39e6628641facfb5bde22fa11c60bc7bb026
as_before <1359521868@qq.com>
2024-10-14 19:13:35 +0800

    静态工厂、代理模式， 编写代码沙箱模块

---
+++ src/main/java/com/web/oj/judge/JudgeService.java
@@ -0,0 +1,16 @@
+package com.web.oj.judge;
+
+import com.web.oj.model.entity.QuestionSubmit;
+
+/**
+ * 判题服务
+ */
+public interface JudgeService {
+
+    /**
+     * 判题
+     * @param questionSubmitId
+     * @return
+     */
+    QuestionSubmit doJudge(long questionSubmitId);
+}
